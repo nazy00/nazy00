05d962a38b181fe70fef9d013d58fde7d712fb82
Sheikh <cybardev@pm.me>
2022-03-19 16:57:04 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,1 @@
 // interactivity
-document.querySelectorAll("details").forEach((element) => {
-    element.addEventListener("click", (event) => {
-        if (event.target.getAttribute("open")) {
-            event.target.children.summary.style.color = "#ff86dd";
-        } else {
-            event.target.children.summary.style.color = initial;
-        }
-    });
-});
